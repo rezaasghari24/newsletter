9c473df0e519ac76087400195422dd143d76f2fa
Reza Asghari <rezaasghari.m.b@gmail.com>
2023-11-19 18:00:00 +0330

    Finished the project

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 const btnSubscribe = document.querySelector('.btn-sub');
 const btnSuccess = document.querySelector('.btn-success');
 const emailNotValid = document.querySelector('.email-not-valid');
@@ -9,27 +8,41 @@ const mainContainer = document.querySelector('.container');
 const containerSuccess = document.querySelector('.container-success');
 
 let emailValue = document.getElementById('email');
-emailValue.value = '';
 let emailDisplay = document.getElementById('display-email');
 
+/* Reset the input box value*/
 const init = function (){
-    emailValue = '';
-    emailValue = document.getElementById('email').value;
+    emailValue.value = '';
 }
 
+/* Validation for Email with Regular Expression */
 const isValidEmail = function(emailValue) {
     const emailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegEx.test(emailValue);
 }
 
+/* Switching the main Container to Success Container */
 const switchContainer = function () {
     mainContainer.classList.toggle('hidden');
     containerSuccess.classList.toggle('hidden');
 }
 
+/* Check Email Address with 'oninput' event */
+const emailOnChange = function(e) {
+    if(isValidEmail(e)) {
+        emailNotValid.classList.add('hidden');
+        emailInput.classList.remove('email-input-not-valid');
+    } else {
+        emailNotValid.classList.remove('hidden');
+        emailInput.classList.add('email-input-not-valid');
+    }
+}
+
+/* Button event */
 btnSubscribe.addEventListener('click', function(){
-    if(isValidEmail(emailValue)) {
-        emailDisplay.textContent = emailValue;
+    const currentEmail = emailValue.value;
+    if(isValidEmail(currentEmail)) {
+        emailDisplay.textContent = currentEmail;
         switchContainer();
         emailNotValid.classList.add('hidden');
         emailInput.classList.remove('email-input-not-valid');
@@ -37,6 +50,8 @@ btnSubscribe.addEventListener('click', function(){
         emailNotValid.classList.remove('hidden');
         emailInput.classList.add('email-input-not-valid');
     }
-    init();
 });
-btnSuccess.addEventListener('click', switchContainer)
+
+init();
+
+btnSuccess.addEventListener('click', switchContainer);
